48722d10278b70d4b2f9f7219c61b3e113d840e8
Timur <t.fralik@aimprosoft.com>
2023-01-08 02:41:08 +0200

    Add Test and Class

--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -3,12 +3,17 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use Timur\Package\Transcription;
 
 class TranscriptionTest extends TestCase
-{
+{ 
     /** @test */
     public function it_loads_vtt_file()
     {
+        $transcription = Transcription::load(__DIR__ . '/stubs/base-example.vtt');
 
+        $expected = file_get_contents(__DIR__ . '/stubs/base-example.vtt');
+
+        $this->assertEquals($expected, $transcription);
     }
 }
