7213407502a576b0d2ee559b294b0dccf8732600
Timur <t.fralik@aimprosoft.com>
2023-01-08 02:16:52 +0200

    Add Boilerplate

---
+++ tests/TranscriptionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TranscriptionTest extends TestCase
+{
+    /** @test */
+    public function it_loads_vtt_file()
+    {
+
+    }
+}
