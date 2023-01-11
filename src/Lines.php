9052d145cf944ac9f08b52b89a93548b66a0a9e5
Timur <t.fralik@aimprosoft.com>
2023-01-11 19:46:41 +0200

    Refactor Collection

--- src/Lines.php
+++ src/Lines.php
@@ -2,72 +2,15 @@
 
 namespace Fralik\Package;
 
-use ArrayAccess;
-use ArrayIterator;
-use Countable;
-use IteratorAggregate;
-use JsonSerializable;
-use Traversable;
-
-class Lines implements Countable, IteratorAggregate, ArrayAccess, JsonSerializable
+class Lines extends Collection
 {
-    private array $lines;
-
-    public function __construct(array $lines)
-    {
-        $this->lines = $lines;
-    }
-
     public function __toString(): string
     {
-        return implode("\n", $this->lines);
+        return implode("\n", $this->items);
     }
 
     public function asHtml(): string
     {
-        $formattedLines = array_map(function (Line $line) {
-                return $line->toAnchorTag();
-            }, $this->lines);
-
-        return (new static($formattedLines))->__toString();
-    }
-
-    public function count(): int
-    {
-        return count($this->lines);
-    }
-
-    public function getIterator(): Traversable
-    {
-        return new ArrayIterator($this->lines);
-    }
-
-    public function offsetExists(mixed $key): bool
-    {
-        return isset($this->lines[$key]);
-    }
-
-    public function offsetGet(mixed $key): mixed
-    {
-        return $this->lines[$key];
-    }
-
-    public function offsetSet(mixed $key, mixed $value): void
-    {
-        if (is_null($key)) {
-            $this->lines[] = $value;
-        } else {
-            $this->lines[$key] = $value;
-        }
-    }
-
-    public function offsetUnset(mixed $key): void
-    {
-        unset($this->lines[$key]);
-    }
-
-    public function jsonSerialize(): mixed
-    {
-        return $this->lines;
+        return $this->map(fn(Line $line) => $line->toAnchorTag())->__toString();
     }
 }
